c421b0dd0b1275edf81e7fb9f9e9c3bfc56183dd
Milesq <wmilosz88@gmail.com>
2020-11-30 23:30:34 +0100

    feat: add Edit view

--- www/src/components/crud/Edit.svelte
+++ www/src/components/crud/Edit.svelte
@@ -1,1 +1,33 @@
-<div>edit</div>
+<div class="card mt-6">
+  <form class="card-content" on:click|preventDefault>
+    <div class="field">
+      <h2 class="subtitle">Edycja</h2>
+
+
+      <!-- svelte-ignore a11y-label-has-associated-control -->
+      <label class="label">Login</label>
+      <div class="control has-icons-left">
+        <input
+          class="input is-info"
+          type="text"
+          placeholder="Login"
+        />
+        <span class="icon is-small is-left">
+          <i class="fas fa-user"></i>
+        </span>
+      </div>
+
+      <button
+        class="
+          my-4
+          button
+          is-info
+          is-outlined
+          is-pulled-right
+          is-transitioned
+        ">
+        Zapisz
+      </button>
+    </div>
+  </form>
+</div>
