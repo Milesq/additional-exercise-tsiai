1a067101aedf658afe9dafa90a929a07a790dcea
Milesq <wmilosz88@gmail.com>
2020-11-30 16:38:44 +0100

    feat: add jumpingText component

---
+++ www/src/components/JumpingText.svelte
@@ -0,0 +1,42 @@
+<div class="text-animation">
+  {#each letters as letter}
+    <div class="text-animation__item">
+      {letter}
+    </div>
+  {/each}
+</div>
+
+<script>
+  /**
+   * @type {string}
+   */
+  export let text = '';
+  $: letters = text.split('');
+</script>
+
+<style lang="scss">
+@use 'sass:list';
+
+$textItemCount: 4;
+
+.text-animation {
+  &__item {
+    display: inline-block;
+    animation: jump .65s ease;
+    margin: auto 2px;
+
+    @for $i from 1 through $textItemCount {
+      &:nth-child(#{$i}) {
+        animation-delay: 0.15s * $i;
+      }
+    }
+  }
+}
+
+@keyframes jump {
+  from { transform: translateY(0) scale(1.25, 0.7); }
+  50% { transform: translateY(-90%) scale(1, 1); }
+  80% { transform: translateY(30%) scale(1.25, .7); }
+  to { transform: translateY(0) scale(1, 1); }
+}
+</style>
