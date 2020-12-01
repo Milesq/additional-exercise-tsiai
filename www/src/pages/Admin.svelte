ddbb7a90b579189bd65f80e82114312b90631f2e
Milesq <wmilosz88@gmail.com>
2020-12-01 22:58:21 +0100

    feat: make logo clickable

--- www/src/pages/Admin.svelte
+++ www/src/pages/Admin.svelte
@@ -1,6 +1,6 @@
 <div class="navbar is-primary is-spaced">
   <div class="navbar-brand">
-    <div class="navbar-item is-size-4">
+    <div class="navbar-item is-size-4 is-clickable" on:click={() => push('/')}>
       <JumpingText text="CRUD" />
     </div>
   </div>
@@ -9,6 +9,7 @@
 <Crud />
 
 <script>
+  import { push } from 'svelte-spa-router';
   import Crud from '../components/Crud.svelte';
   import JumpingText from '../components/JumpingText.svelte';
 </script>
