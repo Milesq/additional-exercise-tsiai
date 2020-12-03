b133724c7372680c41b1d7390d8baf375cd569dd
Milesq <wmilosz88@gmail.com>
2020-12-03 11:22:57 +0100

    feat: add home page layout

---
+++ www/src/components/Quiz.svelte
@@ -0,0 +1,62 @@
+<div class="container">
+  <div class="section">
+    <div class="columns">
+      <div class="column is-6">
+        <div class="notification">
+          <h2 class="title">Najnowsze wiadomości</h2>
+
+          <div class="content">
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Consequuntur eum deleniti unde, distinctio aperiam earum neque
+              aspernatur repellat ad sint et ullam nobis eius! Et natus minima
+              recusandae ducimus temporibus.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Consequuntur eum deleniti unde, distinctio aperiam earum neque
+              aspernatur repellat ad sint et ullam nobis eius! Et natus minima
+              recusandae ducimus temporibus.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Consequuntur eum deleniti unde, distinctio aperiam earum neque
+              aspernatur repellat ad sint et ullam nobis eius! Et natus minima
+              recusandae ducimus temporibus.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column is-6">
+        <div class="notification">
+          <h2 class="title">Testy</h2>
+
+          <div class="content">
+            <p>
+              W trybie nauki, możesz dowolnie przeglądać listę słówek i
+              sprawdzać ich znaczenie
+            </p>
+            <p>
+              W dowolnym momencie możesz zacząć zdobywać punkty dzięki trybowi
+              egzaminu, gdzie będziesz musiał w 20 sekund wpisać prawidłowe
+              znaczenie słówka
+            </p>
+          </div>
+
+          <div class="columns">
+            <div
+              class="column is-4 is-flex is-justify-content-center is-offset-2">
+              <button
+                class="button is-outlined is-transitioned is-rounded is-info">Nauka</button>
+            </div>
+            <div class="column is-4 is-flex is-justify-content-center">
+              <button
+                class="button is-outlined is-transitioned is-rounded is-danger">Egzamin</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- www/src/pages/Home.svelte
+++ www/src/pages/Home.svelte
@@ -1,1 +1,35 @@
-<div>Hello, World</div>
+<div class="navbar is-primary is-spaced">
+  <div class="navbar-brand">
+    <div class="navbar-item is-size-4 normal-cursor">
+      <JumpingText text="ENGLISH" />
+    </div>
+
+    <div class="navbar-burger">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </div>
+  </div>
+
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item has-dropdown is-hoverable">
+        <div class="navbar-link">Wyniki</div>
+
+        <div class="navbar-dropdown">
+          <div class="navbar-item">Poprawnych</div>
+          <hr class="navbar-divider" />
+          <div class="navbar-item">Rzędem: 5</div>
+          <div class="navbar-item">Razem: 19</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<Quiz />
+
+<script>
+  import JumpingText from '../components/JumpingText.svelte';
+  import Quiz from '../components/Quiz.svelte';
+</script>
