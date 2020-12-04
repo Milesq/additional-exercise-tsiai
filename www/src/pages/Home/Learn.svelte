43d25e33bb7b627f5a4e192e25b795c2d5496d88
Milesq <wmilosz88@gmail.com>
2020-12-04 01:12:37 +0100

    feat: show particles after test word

--- www/src/pages/Home/Learn.svelte
+++ www/src/pages/Home/Learn.svelte
@@ -12,14 +12,64 @@
 </div>
 
 <script>
-  import { delay } from '../../utils';
+  import { delay, p } from '../../utils';
+
+  const particles = [
+    {
+      color: 'red',
+      nextBombDelay: 10,
+      disappearance_speed: 0.2,
+      particlesPerPoint: 20,
+      seriesCount: 30,
+      startPoint: {
+        x: 150,
+        y: 150,
+        step: {
+          x: -2,
+          y: -0.8,
+        },
+      },
+    },
+    {
+      color: 'yellow',
+      nextBombDelay: 30,
+      disappearance_speed: 0.075,
+      particlesPerPoint: 20,
+      seriesCount: 30,
+      startPoint: {
+        x: 950,
+        y: 450,
+        step: {
+          x: 2,
+          y: 0.8,
+        },
+      },
+    },
+    {
+      color: 'green',
+      nextBombDelay: 50,
+      disappearance_speed: 0.05,
+      particlesPerPoint: 5,
+      seriesCount: 30,
+      startPoint: {
+        x: 250,
+        y: 500,
+        step: {
+          x: 1,
+          y: 0.4,
+        },
+      },
+    },
+  ];
 
   let isBlured = true;
 
   async function next() {
     isBlured = false;
 
-    await delay(800 + 3000);
+    await delay(800 + 400); // 800ms = time for unblur,
+
+    await p(particles)
 
     isBlured = true;
   }
